231dd88c6c6077fc6c465f8602dd3a18b49bf7cc
samson-en-gert <144666@student.dcterra.nl>
2025-02-10 13:04:17 +0100

    Oefenopdracht 1: User login
    het gaat nu naar de database en Wachtwoordhashing werkt

--- app/index.php
+++ app/index.php
@@ -4,6 +4,32 @@ $host = "mysql";
 $dbname = "my-wonderful-website";
 $charset = "utf8";
 $port = "3306";
+
+spl_autoload_register(function ($class) {
+    include __DIR__ . '/' . $class . '.php';
+});
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $database = new Database();
+    $conn = $database->getConnection();
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+
+    if ($stmt->execute()) {
+        echo "Registratie succesvol!";
+    } else {
+        echo "Er is een fout opgetreden bij de registratie.";
+    }
+}
+
 ?>
 
 <html>
@@ -13,13 +39,19 @@ $port = "3306";
 <title>Registratie</title>
 </head>
 <body>
-    <h2>Registratieformulier</h2>
-    <form action="register.php" method="post">
+
+    <h2>inloggen</h2>
+    <form action="index.php" method="post">
+
         <label for="username">Gebruikersnaam:</label><br>
+
         <input type="text" id="username" name="username" required><br><br>
+
         <label for="password">Wachtwoord:</label><br>
+
         <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" value="Registreren">
+
+        <input type="submit" name="submit" value="Registreren">
     </form>
 
 <body>
